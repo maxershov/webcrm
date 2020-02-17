0c27730474398b0e9d0dd4738b4e142747886233
MaxErshov <maxx95.95@gmail.com>
2020-02-17 15:06:56 +0300

    Add redux logger and del redux devtool

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,21 @@
 /* eslint-disable no-underscore-dangle */
 import { createStore, combineReducers, applyMiddleware , compose} from "redux";
 import thunk from 'redux-thunk';
+// import createSagaMiddlewate from 'redux-saga';
 import dayDataReducer from "./dayData/dayDataReducer";
 import personReducer from "./allPersons/allPersonsReducer";
 import activityReducer from "./activities/ActivitiesReducer";
 import testDataReducer from "./testData/testDataReducer";
 
-// const thunk = ReduxThunk.default;
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// import watchPersons from './testData/testDataActions';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === 'development') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
 
 const store = createStore(
   combineReducers({
@@ -16,11 +24,44 @@ const store = createStore(
     activityStore: activityReducer,
     testDataStore: testDataReducer
   }),
-  composeEnhancers(
-  applyMiddleware(thunk)
-  )
-  // applyMiddleware(thunk)
-  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(applyMiddleware(...middlewares))
 );
 
 export default store;
+
+
+
+// /* eslint-disable no-underscore-dangle */
+// import { createStore, combineReducers, applyMiddleware , compose} from "redux";
+// // import thunk from 'redux-thunk';
+// import createSagaMiddlewate from 'redux-saga';
+// import dayDataReducer from "./dayData/dayDataReducer";
+// import personReducer from "./allPersons/allPersonsReducer";
+// import activityReducer from "./activities/ActivitiesReducer";
+// import testDataReducer from "./testData/testDataReducer";
+
+// import watchPersons from './testData/testDataActions';
+
+// // const thunk = ReduxThunk.default;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const sagaMiddleware = createSagaMiddlewate();
+
+
+// const store = createStore(
+//   combineReducers({
+//     dayDataStore: dayDataReducer,
+//     personStore: personReducer,
+//     activityStore: activityReducer,
+//     testDataStore: testDataReducer
+//   }),
+//   composeEnhancers(
+//   // applyMiddleware(thunk)
+//   applyMiddleware(sagaMiddleware)
+//   )
+//   // applyMiddleware(thunk)
+//   // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+// sagaMiddleware.run(watchPersons)
+
+// export default store;
--- src/store/testData/testDataActions.js
+++ src/store/testData/testDataActions.js
@@ -1,17 +1,14 @@
 import host from "../../../host";
 
 export const requestPersons = () => {
-  console.log("requestPersons");
   return { type: "REQUEST_PERSONS" };
 };
 
 export const getPersons = personsData => {
-  console.log("getPersons");
   return { type: "REQUEST_PERSONS_SUCCEEDED", data: personsData };
 };
 
 export const errorPersons = () => {
-  console.log("fetchError");
   return { type: "REQUEST_PERSONS_FAILED" };
 };
 
@@ -22,7 +19,6 @@ export const fetchPersons = () => {
     .then(res => res.json())
     .then(
       data => {
-        console.log(typeof data, data);
         dispatch(getPersons(data));
       },
       err => dispatch(errorPersons())
