bc62be0de00a9c629d4fee033ed09bd783802290
MaxErshov <maxx95.95@gmail.com>
2020-02-19 15:35:32 +0300

    Fix history table

--- src/Components/TableHistory.jsx
+++ src/Components/TableHistory.jsx
@@ -1,93 +1,102 @@
-import React, { useEffect } from 'react';
-import ReactTable from 'react-table-6/react-table.min';
-import { connect } from 'react-redux';
-import { getDaysLeft } from '../App';
-import Spinner from './Spinner';
-import { fetchActivities } from '../store/activitiesDataStore/activitiesDataActions'
-
+import React, { useEffect } from "react";
+import ReactTable from "react-table-6/react-table.min";
+import { connect } from "react-redux";
+import { getDaysLeft } from "../App";
+import Spinner from "./Spinner";
+import { fetchActivities } from "../store/activitiesDataStore/activitiesDataActions";
 
 // set width to table colums by .className size
 function widthForTable(value) {
-  return Math.round(window.innerWidth * (value / 100))
+  return Math.round(window.innerWidth * (value / 100));
 }
 
-const TableHistory = (props) => {
+const TableHistory = props => {
   console.log(props.activityData);
   console.log(props.loadingActivities);
-  
+
   useEffect(() => {
-    console.log('useEffectTriggered');
+    console.log("useEffectTriggered");
     props.fetchActivities();
-    console.log('useEffecDone');
-    
+    console.log("useEffecDone");
   }, []);
-  // const parsedData = props.activityData.filter(obj => obj.code === props.code);
-  const parsedData = []
+  const parsedData = props.activityData.filter(obj => obj.code === props.code);
 
-    return props.loadingActivities ? (<><Spinner /></>) : (
-      <div className="tableHistory">
-        <ReactTable
-          className="table font_white_shadow -striped -highlight"
-          previousText="Назад"
-          nextText="Вперед"
-          loadingText="Загрузка"
-          noDataText="Нет данных"
-          pageText="Страница"
-          ofText="из"
-          rowsText="профилей"
-          // data={parsedData[0].activity}
-        data={[]}
-          columns={[{
-          Header: 'Тип',
-          accessor: 'type',
-          headerClassName: 'tableHeader',
-          style: { whiteSpace: 'unset' },
-          width: widthForTable(20)
-        }, {
-          Header: 'Дата',
-          accessor: 'date',
-          width: widthForTable(10),
-          headerClassName: 'tableHeader',
-          sortMethod: (a, b) => {
-            const dayA = getDaysLeft(a);
-            const dayB = getDaysLeft(b);
-            return (dayA === "") - (dayB === "") || +(dayA > dayB) || -(dayA < dayB);
+  return props.loadingActivities ? (
+    <>
+      <Spinner />
+    </>
+  ) : (
+    <div className="tableHistory">
+      <ReactTable
+        className="table font_white_shadow -striped -highlight"
+        previousText="Назад"
+        nextText="Вперед"
+        loadingText="Загрузка"
+        noDataText="Нет данных"
+        pageText="Страница"
+        ofText="из"
+        rowsText="профилей"
+        data={parsedData[0]?.activity}
+        columns={[
+          {
+            Header: "Тип",
+            accessor: "type",
+            headerClassName: "tableHeader",
+            style: { whiteSpace: "unset" },
+            width: widthForTable(20)
+          },
+          {
+            Header: "Дата",
+            accessor: "date",
+            width: widthForTable(10),
+            headerClassName: "tableHeader",
+            sortMethod: (a, b) => {
+              const dayA = getDaysLeft(a);
+              const dayB = getDaysLeft(b);
+              return (
+                (dayA === "") - (dayB === "") ||
+                +(dayA > dayB) ||
+                -(dayA < dayB)
+              );
+            }
+          },
+          {
+            Header: "Время",
+            width: widthForTable(10),
+            accessor: "time",
+            headerClassName: "tableHeader"
+          },
+          {
+            Header: "Имя",
+            accessor: "person",
+            style: { whiteSpace: "unset" },
+            width: widthForTable(20),
+            headerClassName: "tableHeader"
+          },
+          {
+            Header: "Значение",
+            accessor: "amount",
+            width: widthForTable(40),
+            headerClassName: "tableHeader",
+            style: { whiteSpace: "unset" }
           }
-        }, {
-          Header: 'Время',
-          width: widthForTable(10),
-          accessor: 'time',
-          headerClassName: 'tableHeader'
-        }, {
-          Header: 'Имя',
-          accessor: 'person',
-          style: { whiteSpace: 'unset' },
-          width: widthForTable(20),
-          headerClassName: 'tableHeader'
-        }, {
-          Header: 'Значение',
-          accessor: 'amount',
-          width: widthForTable(40),
-          headerClassName: 'tableHeader',
-          style: { 'whiteSpace': 'unset' }
-        }]}
-          defaultSorted={[{ id: 'date', desc: true }]}
-          defaultPageSize={10}
-        />
-      </div>
-  )
-}
-
+        ]}
+        defaultSorted={[{ id: "date", desc: true }]}
+        defaultPageSize={10}
+      />
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return {
     activityData: state.activitiesDataStore.data,
     loadingActivities: state.activitiesDataStore.loading
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
-  fetchActivities: () => dispatch(fetchActivities()),
+  fetchActivities: () => dispatch(fetchActivities())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TableHistory);
