1f5c8b461f71cd24d0a98954afe300367a370879
Max Ershov <maxx95.95@gmail.com>
2020-04-20 13:58:26 +0300

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
         <NavLink activeClassName="menu-nav-active" to="/all/page/1">Все</NavLink>
         <NavLink activeClassName="menu-nav-active" to="/clients/page/1">Клиенты</NavLink>
         <NavLink activeClassName="menu-nav-active" to="/lead/page/1">Лид</NavLink>
-        <NavLink activeClassName="menu-nav-active" to="/lost/page/1">Бывшие</NavLink>
+        <NavLink activeClassName="menu-nav-active" to="/lost/page/1">Прошлые</NavLink>
         <NavLink activeClassName="menu-nav-active" to="/employee/page/1">Сотрудники</NavLink>
       </nav>
     </>
