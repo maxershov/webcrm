c8914bb650fda7515a58f9ea243dfb083218ac53
Max Ershov <maxx95.95@gmail.com>
2020-10-15 21:17:29 +0300

    fix: new db create

--- emptyDb/createDb.js
+++ emptyDb/createDb.js
@@ -5,11 +5,12 @@ const homePath = require("../backend/getHomePath");
 const { writeData } = require("../backend/copyDb");
 
 const pathTo = path.join(homePath, "db");
-fs.mkdir(pathTo, (err) => {
+fs.mkdir(path.join(pathTo, "copyData"), { recursive: true }, (err) => {
     if (err) {
         return console.error(err);
     };
+
+    writeData(path.join(homePath, "db", "personData.db"), path.join(__dirname, '../', "emptyDb", "db", "personData.db"));
+    writeData(path.join(homePath, "db", "activityData.db"), path.join(__dirname, '../', "emptyDb", "db", "activityData.db"));
+    writeData(path.join(homePath, "db", "dayData.db"), path.join(__dirname, '../', "emptyDb", "db", "dayData.db"));
 });
-writeData(path.join(homePath, "db", "personData.db"), path.join(__dirname, '../', "emptyDb", "db", "personData.db"));
-writeData(path.join(homePath, "db", "activityData.db"), path.join(__dirname, '../', "emptyDb", "db", "activityData.db"));
-writeData(path.join(homePath, "db", "dayData.db"), path.join(__dirname, '../', "emptyDb", "db", "dayData.db"));
